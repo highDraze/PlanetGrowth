4a5c4c102371ae0a99caa3aed445cae44d33c2cf
Nimaoth <nicolas.otremba@web.de>
2020-06-13 00:09:43 +0200

    added drag and drop playing of cards

--- PlanetGrowth/Assets/Scripts/Card.cs
+++ PlanetGrowth/Assets/Scripts/Card.cs
@@ -7,6 +7,19 @@ public abstract class Card : MonoBehaviour
 {
     public int cost = 1;
     public Vector3 targetPosition;
+    public bool wasClicked = false;
+    public float moveSpeed = 10.0f;
 
     public abstract void Effect();
+
+    void Update()
+    {
+        wasClicked = false;
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, moveSpeed * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        wasClicked = true;
+    }
 }
--- PlanetGrowth/Assets/Scripts/ExampleCard.cs
+++ PlanetGrowth/Assets/Scripts/ExampleCard.cs
@@ -7,5 +7,6 @@ public class ExampleCard : Card
     public override void Effect()
     {
         Debug.Log("Example card played");
+        Destroy(gameObject, 0.5f);
     }
 }
