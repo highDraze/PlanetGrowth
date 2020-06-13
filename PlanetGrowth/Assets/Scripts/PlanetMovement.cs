800436e103a617988cf9838a5019f0153fe0906d
The Spectre <Tristan.n.jesgarz@stud.h-da.de>
2020-06-13 12:08:14 +0200

    world movement : rotation through world x axis, horizontal movement and controls with keys

--- PlanetGrowth/Assets/Scripts/PlanetMovement.cs
+++ PlanetGrowth/Assets/Scripts/PlanetMovement.cs
@@ -7,6 +7,7 @@ public class PlanetMovement : MonoBehaviour
     public LayerMask mouseDragLayer;
     public LayerMask cardLayer;
     public float rotationSpeed = 1.0f;
+    public float horizontalSpeed = 0.1f;
     public float minY = 1.5f;
     public float maxY = 12.5f;
 
@@ -15,33 +16,61 @@ public class PlanetMovement : MonoBehaviour
 
     private bool dragging = false;
 
+
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out var hit, 1000.0f, mouseDragLayer | cardLayer) && hit.transform.gameObject.CompareTag("MouseControl"))
-            {
-                // start dragging
-                lastPosition = hit.point;
-                dragging = true;
-            }
-        }
-        if (Input.GetMouseButtonUp(0))
-            dragging = false;
-        if (dragging && Input.GetMouseButton(0))
-        {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out var hit, 1000.0f, mouseDragLayer) && hit.transform.gameObject.CompareTag("MouseControl"))
-            {
-                var delta = hit.point - lastPosition;
-                transform.position += new Vector3(0, delta.y, 0);
-                transform.rotation *= Quaternion.Euler(0, -delta.x * rotationSpeed, 0);
-                lastPosition = hit.point;
-            }
-        }
 
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
+        if (Input.GetKey("w") | Input.GetKey("s") | Input.GetKey("a") | Input.GetKey("d"))
+        {
+            dragging = false;
+            if (Input.GetKey("w")) {
+                transform.rotation *= Quaternion.Euler(0.1f * rotationSpeed, 0, 0);
+            }
+            else if (Input.GetKey("s")){
+                transform.rotation *= Quaternion.Euler(-0.1f * rotationSpeed, 0, 0);
+            }
+            if (Input.GetKey("a")) {
+                transform.position += new Vector3(-1 * horizontalSpeed, 0, 0);
+            }
+            else if (Input.GetKey("d")) {
+                transform.position += new Vector3(horizontalSpeed, 0, 0);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out var hit, 1000.0f, mouseDragLayer | cardLayer) && hit.transform.gameObject.CompareTag("MouseControl"))
+                {
+                    // start dragging
+                    lastPosition = hit.point;
+                    dragging = true;
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+                dragging = false;
+            if (dragging && Input.GetMouseButton(0))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out var hit, 1000.0f, mouseDragLayer) && hit.transform.gameObject.CompareTag("MouseControl"))
+                {
+                    var delta = hit.point - lastPosition;
+
+                    // Upwards and downward movement of Cursor while dragging if no card is selected
+                    transform.rotation *= Quaternion.Euler(delta.y * rotationSpeed, 0, 0);
+
+                    //transform.position += new Vector3(0, delta.y, 0);
+                    // Horizontal movement of Cursor while dragging if no card is selected
+                    //transform.rotation *= Quaternion.Euler(0, -delta.x * rotationSpeed, 0);
+                    transform.position += new Vector3(delta.x, 0, 0);
+                    lastPosition = hit.point;
+                }
+            }
+
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
+        }
     }
 }
