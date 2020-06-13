6c7ba9b4d735b5e1f71a8b6f8dcf722800339760
Nimaoth <nicolas.otremba@web.de>
2020-06-13 02:44:56 +0200

    added vertical movement script for planet

---
+++ PlanetGrowth/Assets/Scripts/PlanetMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour
+{
+    public LayerMask mouseDragLayer;
+    public LayerMask cardLayer;
+
+    private Vector3 originalPosition;
+    private Vector3 lastPosition;
+
+    private bool dragging = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 1000.0f, mouseDragLayer | cardLayer) && hit.transform.gameObject.CompareTag("MouseControl"))
+            {
+                // start dragging
+                lastPosition = hit.point;
+                dragging = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+            dragging = false;
+        if (dragging && Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 1000.0f, mouseDragLayer) && hit.transform.gameObject.CompareTag("MouseControl"))
+            {
+                var delta = hit.point - lastPosition;
+                transform.position += new Vector3(0, delta.y, 0);
+                lastPosition = hit.point;
+            }
+        }
+    }
+}
