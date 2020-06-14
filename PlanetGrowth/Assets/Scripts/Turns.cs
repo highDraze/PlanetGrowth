f0cc17653f1343af827e36a831d95c9bbc692185
Jnas <jonas.braukmann@icloud.com>
2020-06-14 10:43:18 +0200

    UI Adjustment and Added Value Tags

--- PlanetGrowth/Assets/Scripts/Turns.cs
+++ PlanetGrowth/Assets/Scripts/Turns.cs
@@ -28,8 +28,7 @@ public class Turns : MonoBehaviour
     void Update()
     {
         time -= Time.deltaTime;
-        Mathf.Round(time);
-        currentTimeText.text = time.ToString();
+        currentTimeText.text = time.ToString("#0.0");
         if (0 > time)
             nextTurn();
     }
