72c1d5e78a02d1ca2ffeff97920025cc5854b529
Jnas <jonas.braukmann@icloud.com>
2020-06-14 02:37:27 +0200

    Gaaanz viele Sachen
    Fixes von Tiles, Title Screen usw.

---
+++ PlanetGrowth/Assets/Scripts/IslandBounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandBounce : MonoBehaviour
+{
+    public Transform left;
+    public Transform right;
+
+    public float journeyTime = 600.0f;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+     void Update()
+    {
+        Vector3 center = (left.position + right.position) * 0.5F;
+
+        center -= new Vector3(0, 1, 0);
+
+        Vector3 riseRelCenter = left.position - center;
+        Vector3 setRelCenter = right.position - center;
+
+        float fracComplete = (Time.time - startTime) / journeyTime;
+
+        transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
+        transform.position += center;
+    }
+}
